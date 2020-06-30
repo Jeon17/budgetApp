50263e383cfc6f409a12702d4981b7e66c9c5a0f
Jeon17 <jthomassocial@gmail.com>
2020-06-30 16:17:40 -0400

    changed folder structure

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/heroku_n59pdsdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/heroku_n59pdsdb", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
+
+// routes
+app.use(require("./Develop/routes/api.js"));
 
 const MONGODB_URI =
   process.env.MONGODB_URL || "mongodb://localhost/heroku_n59pdsdb";
@@ -30,8 +33,7 @@ const options = {
 };
 mongoose.connect(MONGODB_URI, options)
 
-// routes
-app.use(require("./routes/api.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
