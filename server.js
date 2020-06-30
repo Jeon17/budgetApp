851c8ac81de7a77a1aa2903c39abd52b31a25e13
Jeon17 <jthomassocial@gmail.com>
2020-06-30 14:29:52 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/heroku_ck6xrn9v", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const MONGODB_URI =
+  process.env.MONGODB_URL || "mongodb://localhost/heroku_ck6xrn9v";
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect(MONGODB_URI, options)
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
