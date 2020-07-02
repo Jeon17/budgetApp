7e27a3958780f1818dc654615fcc2dfb5c942ac9
Jeon17 <jthomassocial@gmail.com>
2020-07-02 11:00:14 -0400

    changed file

--- server.js
+++ server.js
@@ -15,25 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/heroku_n59pdsdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
 // routes
 app.use(require("./Develop/routes/api.js"));
-
-const MONGODB_URI =
-  process.env.MONGODB_URL || "mongodb://localhost/heroku_n59pdsdb";
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4 // Use IPv4, skip trying IPv6
-};
-mongoose.connect(MONGODB_URI, options)
-
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
